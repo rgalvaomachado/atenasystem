88e7fec04e2d8e418f952dc097781f877b76d200
Romulo Galvao <rgalvaomachado@gmail.com>
2022-05-08 16:50:24 -0300

    alteracao no nav menu

--- topMenu.php
+++ topMenu.php
@@ -1,12 +1,8 @@
 <nav class="navbar navbar-custom navbar-fixed-top" role="navigation">
     <div class="container-fluid">
         <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#sidebar-collapse"><span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span></button>
-            <a class="navbar-brand" href="../index.php"><span style="color:orange">Atena</span>System</a>
-            <img src=<?= "/img/hubis.png"?> style="width: 150px; margin-top: 5px;">
+            <a class="navbar-brand" style="position:absolute; width: 150px; margin-top: 5px; left:15px;" href="../index.php"><span style="color:orange">Atena</span>System</a>
+            <a href="http://hubis.com.br/" target="_blank"><img src="/img/hubis.png" style="position:absolute; width: 150px; margin-top: 5px; right:15px;"></a>
         </div>
     </div><!-- /.container-fluid -->
 </nav>
--- tutore/editTutore.php
+++ tutore/editTutore.php
@@ -14,7 +14,114 @@
 <body style="background-color:white">
 	<?php include_once '../topMenu.php'?>
 	<div id="sidebar-collapse" class="col-sm-3 col-lg-2 sidebar">
-		<?php include_once '../navMenu.php'?>
+	<ul class="nav menu">
+			<li class="parent"><a data-toggle="collapse" href="#sub-item-1">
+				<em class="fa fa-user-circle-o" aria-hidden="true">&nbsp;</em> Monitore <span data-toggle="collapse" href="#sub-item-1" class="icon pull-right"><em class="fa fa-plus"></em></span>
+				</a>
+				<ul class="children collapse" id="sub-item-1">
+					<li><a style="background-color:orange;" href="\monitore\cadMonitore.php">
+						<span class="fa fa-arrow-right">&nbsp;</span> Cadastro
+					</a></li>
+					<li><a class="" href="\monitore\editMonitore.php">
+						<span class="fa fa-arrow-right">&nbsp;</span> Editar
+					</a></li>
+				</ul>
+			</li>
+			<li class="parent "><a data-toggle="collapse" href="#sub-item-2">
+				<em class="fa fa-user-o" aria-hidden="true">&nbsp;</em> Tutore <span data-toggle="collapse" href="#sub-item-2" class="icon pull-right"><em class="fa fa-plus"></em></span>
+				</a>
+				<ul class="children collapse in" id="sub-item-2">
+					<li><a class="" href="\tutore\cadTutore.php">
+						<span class="fa fa-arrow-right">&nbsp;</span> Cadastro
+					</a></li>
+					<li><a style="background-color:orange" href="\tutore\editTutore.php">
+						<span class="fa fa-arrow-right">&nbsp;</span> Editar
+					</a></li>
+				</ul>
+			</li>
+			<li class="parent "><a data-toggle="collapse" href="#sub-item-3">
+				<em class="fa fa-graduation-cap" aria-hidden="true">&nbsp;</em> Alune <span data-toggle="collapse" href="#sub-item-3" class="icon pull-right"><em class="fa fa-plus"></em></span>
+				</a>
+				<ul class="children collapse" id="sub-item-3">
+					<li><a class="" href="\alune\cadAlune.php">
+						<span class="fa fa-arrow-right">&nbsp;</span> Cadastro
+					</a></li>
+					<li><a class="" href="\alune\editAlune.php">
+						<span class="fa fa-arrow-right">&nbsp;</span> Editar
+					</a></li>
+					<li><a class="" href="\alune\justificarAlunePresenca.php">
+						<span class="fa fa-arrow-right">&nbsp;</span> Justificar Presença
+					</a></li>
+				</ul>
+			</li>
+			<li class="parent "><a data-toggle="collapse" href="#sub-item-4">
+				<em class="fa fa-newspaper-o" aria-hidden="true">&nbsp;</em> Disciplina <span data-toggle="collapse" href="#sub-item-4" class="icon pull-right"><em class="fa fa-plus"></em></span>
+				</a>
+				<ul class="children collapse" id="sub-item-4">
+					<li><a class="" href="\disciplina\cadDisciplina.php">
+						<span class="fa fa-arrow-right">&nbsp;</span> Cadastro
+					</a></li>
+					<li><a class="" href="\disciplina\editDisciplina.php">
+						<span class="fa fa-arrow-right">&nbsp;</span> Editar
+					</a></li>
+				</ul>
+			</li>
+			<li class="parent "><a data-toggle="collapse" href="#sub-item-5">
+				<em class="fa fa-university" aria-hidden="true">&nbsp;</em> Sala <span data-toggle="collapse" href="#sub-item-5" class="icon pull-right"><em class="fa fa-plus"></em></span>
+				</a>
+				<ul class="children collapse" id="sub-item-5">
+					<li><a class="" href="\sala\cadSala.php">
+						<span class="fa fa-arrow-right">&nbsp;</span> Cadastro
+					</a></li>
+					<li><a class="" href="\sala\editSala.php">
+						<span class="fa fa-arrow-right">&nbsp;</span> Editar
+					</a></li>
+				</ul>
+			</li>
+			<li class="parent "><a data-toggle="collapse" href="#sub-item-6">
+				<em class="fa fa-calendar">&nbsp;</em> Presença <span data-toggle="collapse" href="#sub-item-6" class="icon pull-right"><em class="fa fa-plus"></em></span>
+				</a>
+				<ul class="children collapse" id="sub-item-6">
+					<li><a class="" href="\presenca\presenAlune.php">
+						<span class="fa fa-arrow-right">&nbsp;</span> Aula Alune
+					</a></li>
+					<li><a class="" href="\presenca\presenTutore.php">
+						<span class="fa fa-arrow-right">&nbsp;</span> Aula Tutore
+					</a></li>
+					<li><a class="" href="\presenca\presenMonitore.php">
+						<span class="fa fa-arrow-right">&nbsp;</span> Monitoria
+					</a></li>
+					<li><a class="" href="\presenca\presenReuniao.php">
+						<span class="fa fa-arrow-right">&nbsp;</span> Reunião
+					</a></li>
+				</ul>
+			</li>
+			<li class="parent "><a data-toggle="collapse" href="#sub-item-7">
+				<em class="fa fa-file-pdf-o">&nbsp;</em> Certificado <span data-toggle="collapse" href="#sub-item-7" class="icon pull-right"><em class="fa fa-plus"></em></span>
+				</a>
+				<ul class="children collapse" id="sub-item-7">
+					<li><a class="" href="\certificado\certTutore.php">
+						<span class="fa fa-arrow-right">&nbsp;</span> Tutore
+					</a></li>
+					<li><a class="" href="\certificado\certMonitore.php">
+						<span class="fa fa-arrow-right">&nbsp;</span> Monitore
+					</a></li>
+				</ul>
+			</li>
+			<li class="parent "><a data-toggle="collapse" href="#sub-item-8">
+				<em class="fa fa-bar-chart">&nbsp;</em> Relatorio <span data-toggle="collapse" href="#sub-item-8" class="icon pull-right"><em class="fa fa-plus"></em></span>
+				</a>
+				<ul class="children collapse" id="sub-item-8">
+					<li><a class="" href="\relatorio\relAlune.php">
+						<span class="fa fa-arrow-right">&nbsp;</span> Presença Alune
+					</a></li>
+					<li><a class="" href="\relatorio\relReuniao.php">
+						<span class="fa fa-arrow-right">&nbsp;</span> Precença Reunião
+					</a></li>
+				</ul>
+			</li>
+			<li><a href="../login.html"><em class="fa fa-power-off">&nbsp;</em> Logout</a></li>
+		</ul>
 	</div><!--/.sidebar-->
 		
 	<div class="col-sm-9 col-sm-offset-3 col-lg-10 col-lg-offset-2 main" style="background-color:white; margin-top:100px">
