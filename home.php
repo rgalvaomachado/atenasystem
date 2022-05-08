207ee122acb789fde0c97268aa8f490be38fb0be
Romulo Galvao <rgalvaomachado@gmail.com>
2022-05-08 18:22:06 -0300

    fix icon

--- home.php
+++ home.php
@@ -9,7 +9,7 @@
 	<link href="css/datepicker3.css" rel="stylesheet">
 	<link href="css/styles.css" rel="stylesheet">
 	<link href="https://fonts.googleapis.com/css?family=Montserrat:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
-	<link rel="icon" href="../img/hubis-icon.png">
+	<link rel="icon" href="../img/hubis-icon.png">	<link rel="icon" href="../img/hubis-icon.png">
 </head>
 <body style="background-color:white">
 	<?php include_once 'topMenu.php'?>
