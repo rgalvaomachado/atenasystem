20bbbb08a6811d7582ba0d0d674f8e4b149bacbd
Romulo Galvao <rgalvaomachado@gmail.com>
2022-05-18 21:51:20 -0300

    permisao relatorio

--- menu.php
+++ menu.php
@@ -104,15 +104,17 @@
                     <li><a style=<?= $arquivo == "presenTutore.php" ? "background-color:orange;" : "" ?> class="" href="\presenca\presenTutore.php">
                         <span class="fa fa-arrow-right">&nbsp;</span> Aula Tutore
                     </a></li>
-                    <?php } ?>
+                <?php } ?>
                 <?php if($_SESSION['modo'] == "admin") { ?>
                     <li><a style=<?= $arquivo == "presenMonitore.php" ? "background-color:orange;" : "" ?> class="" href="\presenca\presenMonitore.php">
-                    <span class="fa fa-arrow-right">&nbsp;</span> Monitoria
+                        <span class="fa fa-arrow-right">&nbsp;</span> Monitoria
                     </a></li>
                 <?php } ?>
-                <li><a style=<?= $arquivo == "presenReuniao.php" ? "background-color:orange;" : "" ?> class="" href="\presenca\presenReuniao.php">
-                    <span class="fa fa-arrow-right">&nbsp;</span> Reunião
-                </a></li>
+                <?php if($_SESSION['modo'] == "admin" || $_SESSION['modo'] == "comicao") { ?>
+                    <li><a style=<?= $arquivo == "presenReuniao.php" ? "background-color:orange;" : "" ?> class="" href="\presenca\presenReuniao.php">
+                        <span class="fa fa-arrow-right">&nbsp;</span> Reunião
+                    </a></li>
+                <?php } ?>
             </ul>
         </li>
         <?php if($_SESSION['modo'] == "admin" || $_SESSION['modo'] == "monitore") { ?>
@@ -129,20 +131,22 @@
             </ul>
         </li>
         <?php } ?>
-        <?php if($_SESSION['modo'] == "admin" || $_SESSION['modo'] == "monitore") { ?>
         <li class="parent "><a data-toggle="collapse" href="#sub-item-8">
             <em class="fa fa-bar-chart">&nbsp;</em> Relatorio <span data-toggle="collapse" href="#sub-item-8" class="icon pull-right"><em class="fa fa-plus"></em></span>
             </a>
             <ul class="children collapse <?= $menu == "relatorio" ? "in" : "" ?>" id="sub-item-8">
+                <?php if($_SESSION['modo'] == "admin" || $_SESSION['modo'] == "monitore") { ?>
                 <li><a style=<?= $arquivo == "relAlune.php" ? "background-color:orange;" : "" ?> class="" href="\relatorio\relAlune.php">
                     <span class="fa fa-arrow-right">&nbsp;</span> Presença Alune
                 </a></li>
-                <li><a style=<?= $arquivo == "relReuniao.php" ? "background-color:orange;" : "" ?> class="" href="\relatorio\relReuniao.php">
-                    <span class="fa fa-arrow-right">&nbsp;</span> Precença Reunião
-                </a></li>
+                <?php } ?>
+                <?php if($_SESSION['modo'] == "admin" || $_SESSION['modo'] == "comicao") { ?>
+                    <li><a style=<?= $arquivo == "relReuniao.php" ? "background-color:orange;" : "" ?> class="" href="\relatorio\relReuniao.php">
+                        <span class="fa fa-arrow-right">&nbsp;</span> Precença Reunião
+                    </a></li>
+                <?php } ?>
             </ul>
         </li>
-        <?php } ?>
         <li><a href="../index.php"><em class="fa fa-power-off">&nbsp;</em> Logout</a></li>
     </ul>
 </div>
