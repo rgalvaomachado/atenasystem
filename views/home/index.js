6264071e84e0bc09774376e11e978bfdeec0ff49
rgalvaomachado <rgalvaomachado@gmail.com>
2022-10-20 00:33:37 -0300

    implementacao menu vertical

--- views/home/index.js
+++ views/home/index.js
@@ -40,187 +40,322 @@ function verificaPermissao(){
 
 function menuRepresentante(){
     $('#listMenu').append(`
-        <li><a href="#"><em class="fa fa-user-plus" aria-hidden="true"></em>&nbsp;Representante</a>
+        <li>
+            <input type="checkbox" id="representanteMenu" class="subMenu"/>
+            <label for="representanteMenu">
+                <em class="fa fa-user-plus" aria-hidden="true"></em>&nbsp;Representante
+                <em class="fa fa-plus"></em>
+            </label>
             <ul>
-                <li><a href="#" onclick="cadRepresentante()">Cadastro</a></li>
-                <li><a href="#" onclick="editRepresentante()">Editar</a></li>
-                <li><a href="#" onclick="assinaturaRepresentante()">Assinatura</a></li>
+                <li><a href="#" onclick="cadRepresentante()"><em class="fa fa-arrow-right">&nbsp;</em>Cadastro</a></li>
+                <li><a href="#" onclick="editRepresentante()"><em class="fa fa-arrow-right">&nbsp;</em>Editar</a></li>
+                <li><a href="#" onclick="assinaturaRepresentante()"><em class="fa fa-arrow-right">&nbsp;</em>Assinatura</a></li>
             </ul>
         </li>
     `);
+
     $('#listMenu').append(`
-        <li><a href="#"><em class="fa fa-address-book" aria-hidden="true"></em>&nbsp;Comissão</a>
+        <li>
+            <input type="checkbox" id="comissaoMenu" class="subMenu"/>
+            <label for="comissaoMenu">
+                <em class="fa fa-address-book" aria-hidden="true"></em>&nbsp;Comissão
+                <em class="fa fa-plus"></em>
+            </label>
             <ul>
-                <li><a href="#" onclick="cadComissao()">Cadastro</a></li>
-                <li><a href="#" onclick="editComissao()">Editar</a></li>
+                <li><a href="#" onclick="cadComissao()"><em class="fa fa-arrow-right">&nbsp;</em>Cadastro</a></li>
+                <li><a href="#" onclick="editComissao()"><em class="fa fa-arrow-right">&nbsp;</em>Editar</a></li>
             </ul>
         </li>
     `);
+
     $('#listMenu').append(`
-        <li><a href="#"><em class="fa fa-user-circle-o" aria-hidden="true"></em>&nbsp;Monitore</a>
+        <li>
+            <input type="checkbox" id="monitoreMenu" class="subMenu"/>
+            <label for="monitoreMenu">
+                <em class="fa fa-user-circle-o" aria-hidden="true"></em>&nbsp;Monitore
+                <em class="fa fa-plus"></em>
+            </label>
             <ul>
-                <li><a href="#" onclick="cadMonitore()">Cadastro</a></li>
-                <li><a href="#" onclick="editMonitore()">Editar</a></li>
-                <li><a href="#" onclick="editPresencaMonitore()">Editar Presença</a></li>
+                <li><a href="#" onclick="cadMonitore()"><em class="fa fa-arrow-right">&nbsp;</em>Cadastro</a></li>
+                <li><a href="#" onclick="editMonitore()"><em class="fa fa-arrow-right">&nbsp;</em>Editar</a></li>
+                <li><a href="#" onclick="editPresencaMonitore()"><em class="fa fa-arrow-right">&nbsp;</em>Editar Presença</a></li>
             </ul>
         </li>
     `);
+
     $('#listMenu').append(`
-        <li><a href="#"><em class="fa fa-user-o" aria-hidden="true"></em>&nbsp;Tutore</a>
+        <li>
+            <input type="checkbox" id="tutoreMenu" class="subMenu"/>
+            <label for="tutoreMenu">
+                <em class="fa fa-user-o" aria-hidden="true"></em>&nbsp;Tutore
+                <em class="fa fa-plus"></em>
+            </label>
             <ul>
-                <li><a href="#" onclick="cadTutore()">Cadastro</a></li>
-                <li><a href="#" onclick="editTutore()">Editar</a></li>
-                <li><a href="#" onclick="editPresencaTutore()">Editar Presença</a></li>
-                <li><a href="#" onclick="justificarTutore()">Justificar Presença</a></li>
+                <li><a href="#" onclick="cadTutore()"><em class="fa fa-arrow-right">&nbsp;</em>Cadastro</a></li>
+                <li><a href="#" onclick="editTutore()"><em class="fa fa-arrow-right">&nbsp;</em>Editar</a></li>
+                <li><a href="#" onclick="editPresencaTutore()"><em class="fa fa-arrow-right">&nbsp;</em>Editar Presença</a></li>
+                <li><a href="#" onclick="justificarTutore()"><em class="fa fa-arrow-right">&nbsp;</em>Justificar Presença</a></li>
             </ul>
         </li>
     `);
+
     $('#listMenu').append(`
-        <li><a href="#"><em class="fa fa-graduation-cap" aria-hidden="true"></em>&nbsp;Alune</a>
+        <li>
+            <input type="checkbox" id="aluneMenu" class="subMenu"/>
+            <label for="aluneMenu">
+                <em class="fa fa-graduation-cap" aria-hidden="true"></em>&nbsp;Alune
+                <em class="fa fa-plus"></em>
+            </label>
             <ul>
-                <li><a href="#" onclick="cadAlune()">Cadastro</a></li>
-                <li><a href="#" onclick="editAlune()">Editar</a></li>
-                <li><a href="#" onclick="justificarAlune()">Justificar Presença</a></li>
+                <li><a href="#" onclick="cadAlune()"><em class="fa fa-arrow-right">&nbsp;</em>Cadastro</a></li>
+                <li><a href="#" onclick="editAlune()"><em class="fa fa-arrow-right">&nbsp;</em>Editar</a></li>
+                <li><a href="#" onclick="justificarAlune()"><em class="fa fa-arrow-right">&nbsp;</em>Justificar Presença</a></li>
             </ul>
         </li>
     `);
+
     $('#listMenu').append(`
-        <li><a href="#"><em class="fa fa-newspaper-o" aria-hidden="true"></em>&nbsp;Disciplina</a>
+        <li>
+            <input type="checkbox" id="disciplinaMenu" class="subMenu"/>
+            <label for="disciplinaMenu">
+                <em class="fa fa-newspaper-o" aria-hidden="true"></em>&nbsp;Disciplina
+                <em class="fa fa-plus"></em>
+            </label>
             <ul>
-                <li><a href="#" onclick="cadDisciplina()">Cadastro</a></li>
-                <li><a href="#" onclick="editDisciplina()">Editar</a></li>
+                <li><a href="#" onclick="cadDisciplina()"><em class="fa fa-arrow-right">&nbsp;</em>Cadastro</a></li>
+                <li><a href="#" onclick="editDisciplina()"><em class="fa fa-arrow-right">&nbsp;</em>Editar</a></li>
             </ul>
         </li>
     `);
+
     $('#listMenu').append(`
-        <li><a href="#"><em class="fa fa-university" aria-hidden="true"></em>&nbsp;Sala</a>
+        <li>
+            <input type="checkbox" id="salaMenu" class="subMenu"/>
+            <label for="salaMenu">
+                <em class="fa fa-university" aria-hidden="true"></em>&nbsp;Sala
+                <em class="fa fa-plus"></em>
+            </label>
             <ul>
-                <li><a href="#" onclick="cadSala()">Cadastro</a></li>
-                <li><a href="#" onclick="editSala()">Editar</a></li>
+                <li><a href="#" onclick="cadSala()"><em class="fa fa-arrow-right">&nbsp;</em>Cadastro</a></li>
+                <li><a href="#" onclick="editSala()"><em class="fa fa-arrow-right">&nbsp;</em>Editar</a></li>
             </ul>
         </li>
     `);
+
     $('#listMenu').append(`
-        <li><a href="#"><em class="fa fa-calendar"></em>&nbsp;Presença</a>
+        <li>
+            <input type="checkbox" id="presencaMenu" class="subMenu"/>
+            <label for="presencaMenu">
+                <em class="fa fa-calendar"></em>&nbsp;Presença
+                <em class="fa fa-plus"></em>
+            </label>
             <ul>
-                <li><a href="#" onclick="presencaAlune()">Aula Alune</a></li>
-                <li><a href="#" onclick="presencaTutore()">Aula Tutore</a></li>
-                <li><a href="#" onclick="presencaMonitore()">Monitore</a></li>
-                <li><a href="#" onclick="presencaReuniao()">Reunião</a></li>
+                <li><a href="#" onclick="presencaAlune()"><em class="fa fa-arrow-right">&nbsp;</em>Aula Alune</a></li>
+                <li><a href="#" onclick="presencaTutore()"><em class="fa fa-arrow-right">&nbsp;</em>Aula Tutore</a></li>
+                <li><a href="#" onclick="presencaMonitore()"><em class="fa fa-arrow-right">&nbsp;</em>Monitore</a></li>
+                <li><a href="#" onclick="presencaReuniao()"><em class="fa fa-arrow-right">&nbsp;</em>Reunião</a></li>
             </ul>
         </li>
     `);
+
     $('#listMenu').append(`
-        <li><a href="#"><em class="fa fa-bar-chart"></em>&nbsp;Relatorio</a>
+        <li>
+            <input type="checkbox" id="relatorioMenu" class="subMenu"/>
+            <label for="relatorioMenu">
+                <em class="fa fa-bar-chart"></em>&nbsp;Relatorio
+                <em class="fa fa-plus"></em>
+            </label>
             <ul>
-                <li><a href="#" onclick="relatorioAlune()">Presença Alune</a></li>
-                <li><a href="#" onclick="relatorioReuniao()">Presença Reunião</a></li>
-                <li><a href="#" onclick="relatorioMonitore()">Presença Monitore</a></li>
-                <li><a href="#" onclick="relatorioTutore()">Presença Tutore</a></li>
+                <li><a href="#" onclick="relatorioAlune()"><em class="fa fa-arrow-right">&nbsp;</em>Presença Alune</a></li>
+                <li><a href="#" onclick="relatorioReuniao()"><em class="fa fa-arrow-right">&nbsp;</em>Presença Reunião</a></li>
+                <li><a href="#" onclick="relatorioMonitore()"><em class="fa fa-arrow-right">&nbsp;</em>Presença Monitore</a></li>
+                <li><a href="#" onclick="relatorioTutore()"><em class="fa fa-arrow-right">&nbsp;</em>Presença Tutore</a></li>
             </ul>
         </li>
     `);
+
     $('#listMenu').append(`
-        <li><a href="#"><em class="fa fa-file-pdf-o"></em>&nbsp;Certificado</a>
+        <li>
+            <input type="checkbox" id="certificadoMenu" class="subMenu"/>
+            <label for="certificadoMenu">
+                <em class="fa fa-file-pdf-o"></em>&nbsp;Certificado
+                <em class="fa fa-plus"></em>
+            </label>
             <ul>
-                <li><a href="#" onclick="certificadoTutore()">Tutore</a></li>
-                <li><a href="#" onclick="certificadoMonitore()">Monitore</a></li>
+                <li><a href="#" onclick="certificadoTutore()"><em class="fa fa-arrow-right">&nbsp;</em>Tutore</a></li>
+                <li><a href="#" onclick="certificadoMonitore()"><em class="fa fa-arrow-right">&nbsp;</em>Monitore</a></li>
             </ul>
         </li>
     `);
+
     $('#listMenu').append(`
-        <li><a href="#" onclick="logout()"><em class="fa fa-power-off"></em>&nbsp;Logout</a></li>
+        <li>
+            <input type="checkbox" id="logoutMenu" class="subMenu"/>
+            <label onclick="logout()">
+                <em class="fa fa-power-off"></em>&nbsp;Logout
+            </label>
+        </li>
     `);
 }
 
 function menuMonitore(){
     $('#listMenu').append(`
-        <li><a href="#"><em class="fa fa-user-o" aria-hidden="true"></em>&nbsp;Tutore</a>
+        <li>
+            <input type="checkbox" id="tutoreMenu" class="subMenu"/>
+            <label for="tutoreMenu">
+                <em class="fa fa-user-o" aria-hidden="true"></em>&nbsp;Tutore
+                <em class="fa fa-plus"></em>
+            </label>
             <ul>
-                <li><a href="#" onclick="cadTutore()">Cadastro</a></li>
-                <li><a href="#" onclick="editTutore()">Editar</a></li>
-                <li><a href="#" onclick="editPresencaTutore()">Editar Presença</a></li>
+                <li><a href="#" onclick="cadTutore()"><em class="fa fa-arrow-right">&nbsp;</em>Cadastro</a></li>
+                <li><a href="#" onclick="editTutore()"><em class="fa fa-arrow-right">&nbsp;</em>Editar</a></li>
+                <li><a href="#" onclick="editPresencaTutore()"><em class="fa fa-arrow-right">&nbsp;</em>Editar Presença</a></li>
             </ul>
         </li>
     `);
+
     $('#listMenu').append(`
-        <li><a href="#"><em class="fa fa-graduation-cap" aria-hidden="true"></em>&nbsp;Alune</a>
+        <li>
+            <input type="checkbox" id="aluneMenu" class="subMenu"/>
+            <label for="aluneMenu">
+                <em class="fa fa-graduation-cap" aria-hidden="true"></em>&nbsp;Alune
+                <em class="fa fa-plus"></em>
+            </label>
             <ul>
-                <li><a href="#" onclick="cadAlune()">Cadastro</a></li>
-                <li><a href="#" onclick="editAlune()">Editar</a></li>
-                <li><a href="#" onclick="justificarAlune()">Justificar Presença</a></li>
+                <li><a href="#" onclick="cadAlune()"><em class="fa fa-arrow-right">&nbsp;</em>Cadastro</a></li>
+                <li><a href="#" onclick="editAlune()"><em class="fa fa-arrow-right">&nbsp;</em>Editar</a></li>
+                <li><a href="#" onclick="justificarAlune()"><em class="fa fa-arrow-right">&nbsp;</em>Justificar Presença</a></li>
             </ul>
         </li>
     `);
+
     $('#listMenu').append(`
-        <li><a href="#"><em class="fa fa-newspaper-o" aria-hidden="true"></em>&nbsp;Disciplina</a>
+        <li>
+            <input type="checkbox" id="disciplinaMenu" class="subMenu"/>
+            <label for="disciplinaMenu">
+                <em class="fa fa-newspaper-o" aria-hidden="true"></em>&nbsp;Disciplina
+                <em class="fa fa-plus"></em>
+            </label>
             <ul>
-                <li><a href="#" onclick="cadDisciplina()">Cadastro</a></li>
-                <li><a href="#" onclick="editDisciplina()">Editar</a></li>
+                <li><a href="#" onclick="cadDisciplina()"><em class="fa fa-arrow-right">&nbsp;</em>Cadastro</a></li>
+                <li><a href="#" onclick="editDisciplina()"><em class="fa fa-arrow-right">&nbsp;</em>Editar</a></li>
             </ul>
         </li>
     `);
+
     $('#listMenu').append(`
-        <li><a href="#"><em class="fa fa-university" aria-hidden="true"></em>&nbsp;Sala</a>
+        <li>
+            <input type="checkbox" id="salaMenu" class="subMenu"/>
+            <label for="salaMenu">
+                <em class="fa fa-university" aria-hidden="true"></em>&nbsp;Sala
+                <em class="fa fa-plus"></em>
+            </label>
             <ul>
-                <li><a href="#" onclick="cadSala()">Cadastro</a></li>
-                <li><a href="#" onclick="editSala()">Editar</a></li>
+                <li><a href="#" onclick="cadSala()"><em class="fa fa-arrow-right">&nbsp;</em>Cadastro</a></li>
+                <li><a href="#" onclick="editSala()"><em class="fa fa-arrow-right">&nbsp;</em>Editar</a></li>
             </ul>
         </li>
     `);
+
     $('#listMenu').append(`
-        <li><a href="#"><em class="fa fa-calendar"></em>&nbsp;Presença</a>
+        <li>
+            <input type="checkbox" id="presencaMenu" class="subMenu"/>
+            <label for="presencaMenu">
+                <em class="fa fa-calendar"></em>&nbsp;Presença
+                <em class="fa fa-plus"></em>
+            </label>
             <ul>
-                <li><a href="#" onclick="presencaAlune()">Aula Alune</a></li>
-                <li><a href="#" onclick="presencaTutore()">Aula Tutore</a></li>
+                <li><a href="#" onclick="presencaAlune()"><em class="fa fa-arrow-right">&nbsp;</em>Aula Alune</a></li>
+                <li><a href="#" onclick="presencaTutore()"><em class="fa fa-arrow-right">&nbsp;</em>Aula Tutore</a></li>
             </ul>
         </li>
     `);
+
     $('#listMenu').append(`
-        <li><a href="#"><em class="fa fa-bar-chart"></em>&nbsp;Relatorio</a>
+        <li>
+            <input type="checkbox" id="relatorioMenu" class="subMenu"/>
+            <label for="relatorioMenu">
+                <em class="fa fa-bar-chart"></em>&nbsp;Relatorio
+                <em class="fa fa-plus"></em>
+            </label>
             <ul>
-                <li><a href="#" onclick="relatorioAlune()">Presença Alune</a></li>
-                <li><a href="#" onclick="relatorioTutore()">Presença Tutore</a></li>
+                <li><a href="#" onclick="relatorioAlune()"><em class="fa fa-arrow-right">&nbsp;</em>Presença Alune</a></li>
+                <li><a href="#" onclick="relatorioTutore()"><em class="fa fa-arrow-right">&nbsp;</em>Presença Tutore</a></li>
             </ul>
         </li>
     `);
+
     $('#listMenu').append(`
-        <li><a href="#"><em class="fa fa-file-pdf-o"></em>&nbsp;Certificado</a>
+        <li>
+            <input type="checkbox" id="certificadoMenu" class="subMenu"/>
+            <label for="certificadoMenu">
+                <em class="fa fa-file-pdf-o"></em>&nbsp;Certificado
+                <em class="fa fa-plus"></em>
+            </label>
             <ul>
-                <li><a href="#" onclick="certificadoTutore()">Tutore</a></li>
-                <li><a href="#" onclick="certificadoMonitore()">Monitore</a></li>
+                <li><a href="#" onclick="certificadoTutore()"><em class="fa fa-arrow-right">&nbsp;</em>Tutore</a></li>
+                <li><a href="#" onclick="certificadoMonitore()"><em class="fa fa-arrow-right">&nbsp;</em>Monitore</a></li>
             </ul>
         </li>
     `);
+
     $('#listMenu').append(`
-        <li><a href="#" onclick="logout()"><em class="fa fa-power-off"></em>&nbsp;Logout</a></li>
+        <li>
+            <input type="checkbox" id="logoutMenu" class="subMenu"/>
+            <label onclick="logout()">
+                <em class="fa fa-power-off"></em>&nbsp;Logout
+            </label>
+        </li>
     `);
 }
 
 function menuComissao(){
     $('#listMenu').append(`
-        <li><a href="#"><em class="fa fa-user-o" aria-hidden="true"></em>&nbsp;Tutore</a>
+        <li>
+            <input type="checkbox" id="tutoreMenu" class="subMenu"/>
+            <label for="tutoreMenu">
+                <em class="fa fa-user-o" aria-hidden="true"></em>&nbsp;Tutore
+                <em class="fa fa-plus"></em>
+            </label>
             <ul>
-                <li><a href="#" onclick="justificarTutore()">Justificar Presença</a></li>
+                <li><a href="#" onclick="justificarTutore()"><em class="fa fa-arrow-right">&nbsp;</em>Justificar Presença</a></li>
             </ul>
         </li>
     `);
+
     $('#listMenu').append(`
-        <li><a href="#"><em class="fa fa-calendar"></em>&nbsp;Presença</a>
+        <li>
+            <input type="checkbox" id="presencaMenu" class="subMenu"/>
+            <label for="presencaMenu">
+                <em class="fa fa-calendar"></em>&nbsp;Presença
+                <em class="fa fa-plus"></em>
+            </label>
             <ul>
-                <li><a href="#" onclick="presencaReuniao()">Reunião</a></li>
+                <li><a href="#" onclick="presencaReuniao()"><em class="fa fa-arrow-right">&nbsp;</em>Reunião</a></li>
             </ul>
         </li>
     `);
+
     $('#listMenu').append(`
-        <li><a href="#"><em class="fa fa-bar-chart"></em>&nbsp;Relatorio</a>
+        <li>
+            <input type="checkbox" id="relatorioMenu" class="subMenu"/>
+            <label for="relatorioMenu">
+                <em class="fa fa-bar-chart"></em>&nbsp;Relatorio
+                <em class="fa fa-plus"></em>
+            </label>
             <ul>
-                <li><a href="#" onclick="relatorioReuniao()">Presença Reunião</a></li>
+                <li><a href="#" onclick="relatorioReuniao()"><em class="fa fa-arrow-right">&nbsp;</em>Presença Reunião</a></li>
             </ul>
         </li>
     `);
+
     $('#listMenu').append(`
-        <li><a href="#" onclick="logout()"><em class="fa fa-power-off"></em>&nbsp;Logout</a></li>
+        <li>
+            <input type="checkbox" id="logoutMenu" class="subMenu"/>
+            <label onclick="logout()">
+                <em class="fa fa-power-off"></em>&nbsp;Logout
+            </label>
+        </li>
     `);
 }
 
